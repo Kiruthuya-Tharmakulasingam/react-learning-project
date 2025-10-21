0aca2565b99d6b9cfa83ec698aeda6275990fe53
Kiruthuya <kiruthuyakiruthu@gmail.com>
2025-10-21 16:34:34 +0530

    UseState added

--- src/componensts/hooks/UseState.jsx
+++ src/componensts/hooks/UseState.jsx
@@ -2,12 +2,21 @@ import React, { useState } from 'react'
 
 function UseState() {
     const [count, setCount] = useState(0)
+    const [age, setAge] = useState(20)
 
     function increase() {
         setCount(count + 1)
     }
      function decrease() {
         setCount(count - 1)
+        setAge(age - 1)
+    }
+    function raise() {
+         setAge(age + 1)
+    }
+
+    function reduce() {
+         setAge(age - 1)
     }
   return (
     <>
@@ -20,8 +29,22 @@ function UseState() {
         <button onClick={decrease}>-</button>
         <hr />
     </div>
+    <div>
+        <h1>Age : {age}</h1>
+       <button style={style.container} onClick={raise}>Increase by 1</button> <br />
+       <input type="text" value={age}></input><br />
+       <button style={style.container} onClick={reduce}>Decrease by 1</button><br />
+       <input type="text" value={age}></input><br />
+    </div>
     </>
   )
+}
+
+const style ={
+    container : {
+        backgroundColor : "#ccc",
+        borderRadius : "5px"
+    }
 }
 
 export default UseState
