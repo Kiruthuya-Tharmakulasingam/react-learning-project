e5b599a15b784706e951f98f53835da4719ab702
Kiruthuya <kiruthuyakiruthu@gmail.com>
2025-10-21 16:02:36 +0530

    UseState

---
+++ src/componensts/hooks/UseState.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+function UseState() {
+    const [count, setCount] = useState(0)
+
+    function increase() {
+        setCount(count + 1)
+    }
+     function decrease() {
+        setCount(count - 1)
+    }
+  return (
+    <>
+    <div>
+        <h1>UseState</h1>
+        <button onClick={increase}>+</button>
+        <br />
+        <button>{count}</button>
+        <br />
+        <button onClick={decrease}>-</button>
+        <hr />
+    </div>
+    </>
+  )
+}
+
+export default UseState
