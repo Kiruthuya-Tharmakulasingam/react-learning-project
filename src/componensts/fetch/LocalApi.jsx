f5f00bc671abc8ff515d8645e66dc789d2b1d685
Kiruthuya <kiruthuyakiruthu@gmail.com>
2025-10-23 20:27:40 +0530

    modified

--- src/componensts/fetch/LocalApi.jsx
+++ src/componensts/fetch/LocalApi.jsx
@@ -1,21 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 function LocalApi() {
-    const [books, setBooks] = useState([])
-    
-        useEffect(() => {
-            fetchBooks()
-        }, [])
-    
-        const fetchBooks = async () => {
-          const response = await fetch('https://jsonplaceholder.typicode.com/users')
-          const bookData = await response.json()
-          console.log(bookData)
-          setBooks(bookData)
-        }
-  return (
-    <div>LocalApi</div>
-  )
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = async () => {
+    const response = await fetch("http://localhost:5000/api/books");
+    const bookData = await response.json();
+    console.log(bookData);
+    setBooks(bookData);
+  };
+  return <div>LocalApi</div>;
 }
 
-export default LocalApi
+export default LocalApi;
