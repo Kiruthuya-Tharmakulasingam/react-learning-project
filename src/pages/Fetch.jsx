f9f0d2a990969362ec668a01e2616db487c76972
Kiruthuya <kiruthuyakiruthu@gmail.com>
2025-10-23 15:28:40 +0530

    api connected

--- src/pages/Fetch.jsx
+++ src/pages/Fetch.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Fetch() {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
+    const fetchUsers = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users')
+      const userData = await response.json()
+      console.log(userData)
+      setUsers(userData)
+    }
+
   return (
-    <div>Fetch</div>
+    <div></div>
   )
 }
 
