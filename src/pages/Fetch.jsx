d0de128492e780965465a425736460deaf70c0a2
Kiruthuya <kiruthuyakiruthu@gmail.com>
2025-10-23 16:10:06 +0530

    get api datas in table

--- src/pages/Fetch.jsx
+++ src/pages/Fetch.jsx
@@ -15,7 +15,30 @@ function Fetch() {
     }
 
   return (
-    <div></div>
+    <>
+    <table border={1} cellPadding={8} cellSpacing={0}>
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Website</th>
+        </tr>
+        </thead>
+        <tbody>
+            {users.map((user) => (
+            <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+                <td>{user.website}</td>
+            </tr>
+            ))}
+        </tbody>
+    </table>
+    </>
   )
 }
 
