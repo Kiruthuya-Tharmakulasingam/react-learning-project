eb19ae8cd661e4031bbb76b4d21e683d72bf8bac
Kiruthuya <kiruthuyakiruthu@gmail.com>
2025-10-28 09:45:51 +0530

    imported components

---
+++ src/pages/Axios.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FetchData from '../componensts/axios/FetchData'
+import PostData from '../componensts/axios/PostData'
+
+function Axios() {
+  return (
+    <>
+    <PostData />
+    <FetchData />
+    </>
+  )
+}
+
+export default Axios
